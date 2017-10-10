663af17b4d9ab9cb26549ce3bb6c39e24e42b720
alberto.jacinto <alberto.jacinto@UTWKS0567.liveu-tad.es>
2017-10-10 13:20:24 +0200

    Aleatorios hecho

--- PSPR_UD01_T03/src/Hijo/Aleatorios.java
+++ PSPR_UD01_T03/src/Hijo/Aleatorios.java
@@ -3,17 +3,16 @@ package Hijo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Aleatorios {
 
 	public static void main(String[] args) throws IOException {
-		int randomNum;
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String st = null;
-		st = in.readLine();
+		String st = in.readLine();
+		
 		while (!st.equals("")) {
-			randomNum = (int) Math.floor(Math.random() * (10 - 0 + 1) + (0));
-			System.out.println(randomNum);
+			System.out.println(ThreadLocalRandom.current().nextInt(0, 10 + 1));
 			st = in.readLine();
 		}
 	}
--- PSPR_UD01_T03/src/Padre/AleatoriosPadre.java
+++ PSPR_UD01_T03/src/Padre/AleatoriosPadre.java
@@ -6,27 +6,23 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class AleatoriosPadre {
-	public static void main(String[] args) {
-		try {
-			ProcessBuilder pb = new ProcessBuilder("java", "-jar", "Hijo/HijoAleatorio.jar");
-			Process pc = pb.start();
+	public static void main(String[] args) throws IOException {
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", "./lib/Aleatorios.jar");
+		Process pc = pb.start();
 
-			BufferedReader brPadre = new BufferedReader(new InputStreamReader(System.in));
-			BufferedReader brHijo = new BufferedReader(new InputStreamReader(pc.getInputStream()));
+		BufferedReader brPadre = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader brHijo = new BufferedReader(new InputStreamReader(pc.getInputStream()));
 
-			PrintWriter pwHijo = new PrintWriter(pc.getOutputStream());
+		PrintWriter pwHijo = new PrintWriter(pc.getOutputStream());
 
-			String stPadre = brPadre.readLine();
+		String stPadre = brPadre.readLine();
 
-			while (!stPadre.equals("fin")) {
-				pwHijo.println(stPadre);
-				pwHijo.flush();
-				System.out.println(brHijo.readLine());
-				stPadre = brPadre.readLine();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		while (!stPadre.equals("fin")) {
+			pwHijo.println(stPadre);
+			pwHijo.flush();
+			String st2 = brHijo.readLine();
+			System.out.println(st2);
+			stPadre = brPadre.readLine();
 		}
 	}
 }
