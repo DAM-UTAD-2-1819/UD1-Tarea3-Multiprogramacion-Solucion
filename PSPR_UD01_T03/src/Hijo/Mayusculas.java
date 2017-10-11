b090428908bfe4ce3731b8c5e765655245358f35
Alejandro Quirós <alejandroquiros@localhost>
2017-10-11 10:14:40 +0200

    2 acabado a medias

--- PSPR_UD01_T03/src/Hijo/Mayusculas.java
+++ PSPR_UD01_T03/src/Hijo/Mayusculas.java
@@ -3,18 +3,18 @@ package Hijo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Mayusculas {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String st = in.readLine();
+		InputStreamReader in= new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(in);
 		
-		while (!st.equals("")) {
-			String mayus = st.toUpperCase();
-			System.out.println(mayus);
-			st = in.readLine();
+		try{
+			System.out.print(br.readLine().toUpperCase());
+		}
+		catch(IOException e){
+			e.printStackTrace();
 		}
 	}
 }
